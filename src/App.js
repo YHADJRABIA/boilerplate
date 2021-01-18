5fa7052c6b6e7e24e6c0a6867d128585b7f89d26
YHADJRABIA <h_yacine@live.fr>
2021-01-18 23:27:54 +0100

    Added content.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./styles/app.scss";
+
+/* Fixed elements */
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+
+/* Pages */
+import Home from "./components/Pages/Home";
+/* import Register from "./components/Pages/Register";
+import Login from "./components/Pages/Login";
+import Account from "./components/Pages/Account"; */
+
 import i18n from "./i18n/i18n"; // Translating content
 import Loading from "./components/Others/Loading";
 import { DataProvider } from "./components/Context/DataContext";
@@ -12,34 +21,34 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   // Executes when the component has finished rendering
-  useEffect(() => {
+  /*   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 1000);
   }, []);
-
   if (loading) return <Loading />;
-  else
-    return (
-      <>
-        {/* Passes data to all wrapped components. */}
-        <DataProvider>
-          {/* Enables routing with all wrapped components. */}
-          <Header />
-          <Main />
-          <Footer />
-        </DataProvider>
+  else */
+  return (
+    <>
+      {/* Passes data to all wrapped components. */}
+      <DataProvider>
+        {/* Enables routing with all wrapped components. */}
         <Router>
-          {/* Enables the rendering of one component only. */}
-          <Switch>
-            {/*         <Route path={["/", "/home"]} exact component={Home}></Route>
-            <Route path="/about" component={About}></Route>
-            <Route path="/shop" exact component={Shop}></Route>
-            <Route path="/shop/:id" component={Item}></Route> */}
-          </Switch>
+          <Header />
+          <main>
+            {/* Enables the rendering of one component only. */}
+            <Switch>
+              <Route path={["/", "/home"]} exact component={Home}></Route>
+              {/*               <Route path={"/registration"} exact component={Register}></Route>
+              <Route path={"/login"} exact component={Login}></Route>
+              <Route path={"/account"} exact component={Account}></Route> */}
+            </Switch>
+          </main>
+          <Footer />
         </Router>
-      </>
-    );
+      </DataProvider>
+    </>
+  );
 }
 
 export default App;
